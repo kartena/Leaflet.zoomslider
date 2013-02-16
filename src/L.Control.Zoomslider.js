59cd3ca7e08c577fa61ef98542d5be717d045b81
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2013-02-16 10:21:43 +0100

    Drop some now useless lines of code.

--- src/L.Control.Zoomslider.js
+++ src/L.Control.Zoomslider.js
@@ -1,106 +1,51 @@
 L.Control.Zoomslider = (function(){
 
-	// TODO: remove some cpu cycles
 	var Knob = L.Draggable.extend({
 		initialize: function (element, steps, stepHeight) {
+			var sliderHeight = steps * stepHeight,
+				knobHeight = 5 ; // element.offsetHeight; // TODO: Not inited yet. fix
 			L.Draggable.prototype.initialize.call(this, element, element);
 
 			this._element = element;
 			this._maxValue = steps - 1;
-			this._initConversion(steps, stepHeight);
+
+			// conversion parameters
+			this._k = -stepHeight;
+			this._m = sliderHeight - (stepHeight + knobHeight) / 2;
 
 			this.on('predrag', function() {
 				this._newPos.x = 0;
 				this._newPos.y = this._adjust(this._newPos.y);
 			}, this);
 		},
-		_initConversion: function (steps, stepHeight) {
-			var sliderHeight = steps * stepHeight,
-				knobHeight = 5 ; // this._element.offsetHeight; // TODO: Not inited yet. fix
-			this._knobOffset = (stepHeight + knobHeight) / 2;
-			this._k = -stepHeight;
-			this._m = sliderHeight - this._knobOffset;
-		},
+
 		_adjust: function (y) {
-			var value = Math.round(this._yToValue(y));
+			var value = Math.round(this._toValue(y));
 			value = Math.max(0, Math.min(this._maxValue, value));
-			return this._valueToY(value);
+			return this._toY(value);
 		},
+
 		// y = k*v + m
-		_yToValue: function (y){
+		_toValue: function (y) {
 			return (y - this._m) / this._k;
 		},
 		// v = (y - m) / k
-		_valueToY: function(v){
+		_toY: function (v) {
 			return this._k * v + this._m;
 		},
 
-		setPosition: function(y){
+		setPosition: function (y) {
 			L.DomUtil.setPosition(this._element,
 								  L.point(0, this._adjust(y)));
 		},
 
 		setValue: function (v) {
-			var y = this._valueToY(v);
-			L.DomUtil.setPosition(this._element, L.point(0, y));
+			L.DomUtil.setPosition(this._element,
+								  L.point(0, this._toY(v)));
 		},
 
 		getValue: function () {
-			var y = L.DomUtil.getPosition(this._element).y;
-			return this._yToValue(y);
-		}
-	});
-
-	var Slider = L.Class.extend({
-		includes: L.Mixin.Events,
-
-		options: {
-			stepHeight: 9,
-			styleNS: "leaflet-control-zoomslider-slider"
-		},
-
-		initialize: function (element, steps, value) {
-			this._steps = steps;
-			this._height = this.options.stepHeight * steps;
-
-			this._body = L.DomUtil.create('div',
-										  this.options.styleNS + '-body',
-										  element);
-			this._body.style.height = this._height + "px";
-			L.DomEvent.on(this._body, 'click', this._onClick, this);
-			this._knob = this._createKnob(this._body);
-			this._knob.enable();
-
-			this.setValue(value);
-		},
-
-		setValue: function (value) {
-			this._knob.setValue(value);
-		},
-		getValue: function() {
-			return this._knob.getValue();
-		},
-		getSteps: function(){
-			return this._steps;
-		},
-
-		_createKnob: function (parent) {
-			var elem = L.DomUtil.create('div', this.options.styleNS + '-knob', parent);
-			L.DomEvent.disableClickPropagation(elem);
-
-			return new Knob(elem, this._steps, this.options.stepHeight)
-				.on('dragend', function () {
-					this.fire("update", { value: this._knob.getValue() } );
-				}, this);
-		},
-
-		_onClick: function (e) {
-			var first = (e.touches && e.touches.length === 1 ? e.touches[0] : e);
-			var y = L.DomEvent.getMousePosition(first).y
-					- L.DomUtil.getViewportOffset(this._body).y; // Cache this?
-
-			this._knob.setPosition(y);
-			this.fire("update", { value: this._knob.getValue() } );
+			return this._toValue(L.DomUtil.getPosition(this._element).y);
 		}
 	});
 
@@ -134,7 +79,6 @@ L.Control.Zoomslider = (function(){
 				.on("zoomend", this._updateSlider, this)
 				.on("zoomend", this._updateDisabled, this)
 				.whenReady(this._createSlider, this)
-				//.whenReady(this._updateSlider, this)
 				.whenReady(this._updateDisabled, this);
 
 			return container;
@@ -147,29 +91,40 @@ L.Control.Zoomslider = (function(){
 		},
 
 		_refresh: function () {
-			// TODO: just refresh the slider
-			if(!this._slider || this._slider.getSteps() !== this._zoomLevels() ){
-				this._map
-					.removeControl(this)
-					.addControl(this);
-			}
+			// TODO: listen to zoomlevelschange-event instead in 0.6.x
+			this._map
+				.removeControl(this)
+				.addControl(this);
 		},
 		_zoomLevels: function(){
 			return this._map.getMaxZoom() - this._map.getMinZoom() + 1;
 		},
 
 		_createSlider: function () {
-			var zoomLevels = this._zoomLevels();
+			var bodyElem,
+				knobElem,
+				zoomLevels = this._zoomLevels();
+
 			// This means we have no tilelayers (or that they are setup in a strange way).
 			// Either way we don't want to add a slider here.
 			if(zoomLevels == Infinity){
 				return;
 			}
-			this._slider = new Slider(
-				this._sliderElem,
-				zoomLevels,
-				this._toSliderValue(this._map.getZoom())
-			).on("update", this._updateZoom, this);
+
+			bodyElem = L.DomUtil.create('div',
+									this.options.styleNS + '-body',
+									this._sliderElem);
+			bodyElem.style.height = (this.options.stepHeight * zoomLevels) + "px";
+			L.DomEvent.on(bodyElem, 'click', this._onClick, this);
+
+			knobElem = L.DomUtil.create('div', this.options.styleNS + '-knob', bodyElem);
+			L.DomEvent.disableClickPropagation(knobElem);
+
+			this._knob = new Knob(knobElem, zoomLevels, this.options.stepHeight)
+				.on('dragend', function () {
+					this._map.setZoom(this._toZoomLevel(this._knob.getValue()));
+				}, this)
+				.enable();
 		},
 
 		_zoomIn: function (e) {
@@ -201,13 +156,8 @@ L.Control.Zoomslider = (function(){
 		_toSliderValue: function (zoomLevel) {
 			return zoomLevel - this._map.getMinZoom();
 		},
-		_updateZoom: function(e){
-			this._map.setZoom(this._toZoomLevel(e.value));
-		},
 		_updateSlider: function(){
-			if(this._slider){
-				this._slider.setValue(this._toSliderValue(this._map.getZoom()));
-			}
+			this._knob.setValue(this._toSliderValue(this._map.getZoom()));
 		},
 		_updateDisabled: function () {
 			var map = this._map,
